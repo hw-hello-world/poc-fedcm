3968e80673f35a3f7bcfc4fc89acceffdaed1705
Haisheng Wu <haisheng.wu@okta.com>
2022-04-19 11:44:48 -0700

    add a bit server

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,13 +10,13 @@ router.get('/fedcm.json', function(req, res, next) {
   const resp = {
     "accounts_endpoint": "https://localhost:3000/fedcm/accounts_endpoint",
     "client_metadata_endpoint": "https://localhost:3000/fedcm/client_metadata_endpoint",
-    "id_token_endpoint": "https://localhost:3000/fedcm/token_endpoint",
+    "id_token_endpoint": "https://localhost:3000/fedcm/id_token_endpoint",
     "revocation_endpoint": "https://localhost:3000/fedcm/revocation_endpoint",
     "branding": {
       "background_color": "green",
       "color": "#FFEEAA",
       "icons": [{
-        "url": "https://localhost:3000/icon.ico",
+        "url": "https://localhost:3000/images/logo.ico",
         "size": 10
       }]
     }
@@ -24,8 +24,24 @@ router.get('/fedcm.json', function(req, res, next) {
   res.json(resp);
 });
 
-router.get('/fedcm/accounts_endpoint', function(req, res, next) {});
-router.get('/fedcm/client_metadata_endpoint', function(req, res, next) {});
-router.get('/fedcm/token_endpoint', function(req, res, next) {});
-router.get('/fedcm/revocation_endpoint', function(req, res, next) {});
+router.get('/fedcm/accounts_endpoint', function(req, res, next) {
+  console.log(req);
+  res.json([]);
+});
+
+router.get('/fedcm/client_metadata_endpoint', function(req, res, next) {
+  console.log(req);
+});
+
+router.get('/fedcm/id_token_endpoint', function(req, res, next) {
+  console.log(req);
+  res.json({
+    id_token: 'test id token',
+  });
+});
+
+router.get('/fedcm/revocation_endpoint', function(req, res, next) {
+  console.log(req);
+});
+
 module.exports = router;
